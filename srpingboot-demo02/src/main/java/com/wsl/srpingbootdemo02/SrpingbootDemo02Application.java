7258a13adbd62e2716b49a51a44d2745a7ccf3f1
apple <abc>
2022-05-08 23:24:32 +0800

    init

---
+++ srpingboot-demo02/src/main/java/com/wsl/srpingbootdemo02/SrpingbootDemo02Application.java
@@ -0,0 +1,13 @@
+package com.wsl.srpingbootdemo02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrpingbootDemo02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SrpingbootDemo02Application.class, args);
+    }
+
+}
---
+++ srpingboot-demo02/src/test/java/com/wsl/srpingbootdemo02/SrpingbootDemo02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wsl.srpingbootdemo02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrpingbootDemo02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
